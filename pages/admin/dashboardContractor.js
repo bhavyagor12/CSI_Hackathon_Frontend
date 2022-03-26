55a34e19baa9a477fb12ecfbf99adb1ab7c38b98
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-03-26 17:07:46 +0530

    something

--- pages/admin/dashboardContractor.js
+++ pages/admin/dashboardContractor.js
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "../../components/admin/Sidebar.js";
 import Navbar from "../../components/admin/Navbar.js";
 import Form from "../../components/admin/Projects";
+
 function dashboard() {
   return (
     <div className="flex ">
--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
 import Particle from "../components/Particles";
 import Firework from "../components/Firework";
@@ -11,20 +11,24 @@ import {
   FaRegEnvelope,
   FaLock,
 } from "react-icons/fa";
-import {firebase,auth} from '../firebase/initFirebase';
-import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { firebase, auth } from "../firebase/initFirebase";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 
 const googleprovider = new GoogleAuthProvider();
 
-const googlelogin=()=>{
-  signInWithPopup(auth,googleprovider)
-  .then((result)=>{
-      const credential= GoogleAuthProvider.credentialFromResult(result);
-      const token= credential.accessToken;
+const googlelogin = () => {
+  signInWithPopup(auth, googleprovider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
       const user = result.user;
-      window.location="/";
-  })
-  .catch((error) => {
+      window.location = "/";
+    })
+    .catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -34,33 +38,29 @@ const googlelogin=()=>{
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
-}
+};
 
-const emaillogin=(email,password)=>{
-  console.log(email+password)
+const emaillogin = (email, password) => {
+  console.log(email + password);
   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-      // Signed in 
+    .then((userCredential) => {
+      // Signed in
       const user = userCredential.user;
-      window.location="/";
+      window.location = "/";
       // ...
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
       alert("Error signing up");
       const errorCode = error.code;
       const errorMessage = error.message;
-      window.location="/";
+      window.location = "/";
       // ..
-  });
-}
-
-
+    });
+};
 
 const login = () => {
-  const [EmailID, setEmailID]=useState("");
-  const [Password, setPassword]=useState("");
-
-
+  const [EmailID, setEmailID] = useState("");
+  const [Password, setPassword] = useState("");
 
   return (
     <div className="min-h-screen bg-[#0082EF] dark:bg-[#032A5D]">
@@ -81,7 +81,10 @@ const login = () => {
                 </h2>
                 <div className="mb-2 inline-block w-10 border-2 border-[#0082EF]"></div>
                 <div className="my-2 flex justify-center ">
-                  <button className="border-2 border-gray-200 rounded-full p-3 mx-1 hover:bg-[#0082EF]" onClick={()=>googlelogin()}>
+                  <button
+                    className="border-2 border-gray-200 rounded-full p-3 mx-1 hover:bg-[#0082EF]"
+                    onClick={() => googlelogin()}
+                  >
                     <FaGoogle className="text-xl" />
                   </button>
                 </div>
@@ -95,8 +98,7 @@ const login = () => {
                       placeholder="Email"
                       className="bg-gray-100 outline-none text-sm flex-1"
                       value={EmailID}
-                      onChange={(e)=>setEmailID(e.target.value)}
-
+                      onChange={(e) => setEmailID(e.target.value)}
                     />
                   </div>
                   <div className="bg-gray-100 w-64 p-2 flex items-center ">
@@ -107,10 +109,15 @@ const login = () => {
                       placeholder="Password"
                       className="bg-gray-100 outline-none text-sm flex-1"
                       value={Password}
-                      onChange={(e)=>setPassword(e.target.value)}
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
-                  <button className="mt-10 inline-block rounded-full border-2 bg-[#0082EF] border-white px-12 py-2 font-semibold hover:bg-black hover:text-[#0082EF]" onClick={()=>{emaillogin(EmailID,Password)}}>
+                  <button
+                    className="mt-10 inline-block rounded-full border-2 bg-[#0082EF] border-white px-12 py-2 font-semibold hover:bg-black hover:text-[#0082EF]"
+                    onClick={() => {
+                      emaillogin(EmailID, Password);
+                    }}
+                  >
                     Sign in
                   </button>
                 </div>
