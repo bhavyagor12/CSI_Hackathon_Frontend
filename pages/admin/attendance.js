ae40ccdc50d1206fe72a4a57d3efd599ba5c59ae
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-03-26 21:10:21 +0530

    m

--- pages/admin/attendance.js
+++ pages/admin/attendance.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Sidebar from "../../components/admin/Sidebar.js";
 import AttendanceTable from "../../components/admin/AttendanceTable.js";
+import { VictoryPie, VictoryChart } from "victory";
+
 const attendance = () => {
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 },
+  ];
   return (
-    <div className="flex ">
+    <div className="flex bg-gray-900   ">
       <Sidebar />
       <AttendanceTable />
+      <VictoryPie
+        data={data}
+        x="quarter"
+        y="earnings"
+        colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
+      />
     </div>
   );
 };
