52bf1cb4c944c858ca93bb7e7d12850e925c8508
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-03-26 21:41:13 +0530

    table

--- components/admin/ProjectTable.js
+++ components/admin/ProjectTable.js
@@ -1,72 +1,34 @@
 import React, { useEffect,useState } from 'react';
-import { collection, doc, getCollection } from "firebase/firestore";
+import { collection, doc, getCollection, getDocs, query } from "firebase/firestore";
 import 'firebase/firestore';
 import firebase from "../../firebase/initFirebase";
 import { getFirestore } from "firebase/firestore";
+import { async } from '@firebase/util';
 
 
 
 
 
 const ProjectTable = () => {
-
-    const [info , setInfo] = useState([]);
-    window.addEventListener('load', () => {
-        Fetchdata();
-      });
-
-    const Fetchdata = ()=>{
-    db.collection("Projects").get().then((querySnapshot) => {
-             
-            // Loop through the data and store
-            // it in array to display
-            querySnapshot.forEach(element => {
-                var data = element.data();
-                setInfo(arr => [...arr , data]);
-                  
-            });
-        })
+    useEffect(() => {
+        Fetchdata()
+    }, [])
+    const Fetchdata = async()=>{
+        const db = getFirestore(firebase);
+        const q = query(collection(db, "Projects"));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+});
     }
 
-
-    const db = getFirestore(firebase);
-    const projectRef = collection(db, "Projects");
-    const snapshot = getCollection(projectRef)
-    console.log(snapshot);
-    
-    
     return (
         <div>
-            <div className="container mx-auto px-4 sm:px-8">
-                <h1>haaa</h1>
-            </div>
-            {
-            info.map((data) => (
-            <Frame course={data.ProjectName} 
-                   ProjectName={data.ProjectName} 
-                   ClientName={data.ClientName}
-                   
-                   />
-            ))
-        }
+            
         </div>
     )
 }
-const Frame = ({ ProjectName , ClientName}) => {
-    console.log(+ ProjectName + " " + ClientName);
-    return (
-        <center>
-            <div className="div">
-                  
-<p>Project Name : {ProjectName}</p>
-   
-                  
-<p>Client Name : {ClientName}</p>
-  
-   
-            </div>
-        </center>
-    );
-}
+
 
 export default ProjectTable
