8cd7522bf8f86dd1a910a999c8ea0d469393319d
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-03-26 16:32:00 +0530

    projects added

--- pages/admin/dashboardContractor.js
+++ pages/admin/dashboardContractor.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Sidebar from "../../components/admin/Sidebar.js";
 import Navbar from "../../components/admin/Navbar.js";
+import Form from "../../components/admin/Projects";
 function dashboard() {
   return (
     <div className="flex ">
       <Sidebar />
-      <Navbar />
     </div>
   );
 }
