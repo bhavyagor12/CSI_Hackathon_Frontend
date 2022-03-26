735ee97333d07078d0da51db33ae0e206c09a7cf
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-03-26 17:11:47 +0530

    Update Projects.js

--- components/admin/Projects.js
+++ components/admin/Projects.js
@@ -37,36 +37,36 @@ function Projects() {
         </div>
       </div>
       <div className="my-10 flex justify-center">
-        <form class="w-full max-w-lg">
-          <div class="-mx-3 mb-6 flex flex-wrap">
-            <div class="mb-6 w-full px-3 md:mb-0 md:w-1/2">
+        <form className="w-full max-w-lg">
+          <div className="-mx-3 mb-6 flex flex-wrap">
+            <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
               <label
-                class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
-                for="grid-first-name"
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                
                 value={ProjectName}
                 onChange={(e)=>setProjectName(e.target.value)}
               >
                 Project Name
               </label>
               <input
-                class="mb-3 block w-full appearance-none rounded border border-red-500 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:bg-white focus:outline-none"
+                className="mb-3 block w-full appearance-none rounded border border-red-500 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:bg-white focus:outline-none"
                 id="grid-first-name"
                 type="text"
                 placeholder=""
               />
-              <p class="text-xs italic text-red-500">
+              <p className="text-xs italic text-red-500">
                 Please fill out this field.
               </p>
             </div>
-            <div class="w-full px-3 md:w-1/2">
+            <div className="w-full px-3 md:w-1/2">
               <label
-                class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
-                for="grid-last-name"
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                
               >
                 Client Name
               </label>
               <input
-                class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
                 id="grid-last-name"
                 type="text"
                 placeholder=""
@@ -75,37 +75,36 @@ function Projects() {
               />
             </div>
           </div>
-          <div class="-mx-3 mb-6 flex flex-wrap">
-            <div class="w-full px-3">
+          <div className="-mx-3 mb-6 flex flex-wrap">
+            <div className="w-full px-3">
               <label
-                class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
-                for="grid-password"
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
+                
               >
                 Description
               </label>
               <input
-                class="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                className="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
                 id="grid-password"
                 type="text"
                 placeholder=""
                 value={Description}
                 onChange={(e)=>setDescription(e.target.value)}
               />
-              <p class="text-xs italic text-gray-600">
+              <p className="text-xs italic text-gray-600">
                 A DETAILED DESC IS HELPFUL
               </p>
             </div>
           </div>
-          <div class="-mx-3 mb-2 flex flex-wrap">
-            <div class="mb-6 w-full px-3 md:mb-0 md:w-1/3">
+          <div className="-mx-3 mb-2 flex flex-wrap">
+            <div className="mb-6 w-full px-3 md:mb-0 md:w-1/3">
               <label
-                class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
-                for="grid-city"
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
               >
                 Total Workers
               </label>
               <input
-                class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
                 id="grid-city"
                 type="numbers"
                 placeholder=""
@@ -113,15 +112,14 @@ function Projects() {
                 onChange={(e)=>setTotalWorkers(e.target.value)}
               />
             </div>
-            <div class="mb-6 w-full px-3 md:mb-0 md:w-1/3">
+            <div className="mb-6 w-full px-3 md:mb-0 md:w-1/3">
               <label
-                class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
-                for="grid-zip"
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
               >
                 Start date
               </label>
               <input
-                class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
                 id="grid-zip"
                 type="date"
                 placeholder=""
@@ -130,15 +128,14 @@ function Projects() {
               />
             </div>
 
-            <div class="mb-6 w-full px-3 md:mb-0 md:w-1/3">
+            <div className="mb-6 w-full px-3 md:mb-0 md:w-1/3">
               <label
-                class="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
-                for="grid-zip"
+                className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700"
               >
                 Duration of contract
               </label>
               <input
-                class="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
+                className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
                 id="grid-zip"
                 type="text"
                 placeholder=""
@@ -146,7 +143,7 @@ function Projects() {
                 onChange={(e)=>setDuration(e.target.value)}
               />
             </div>
-            <div class="m-6 mb-6 w-full px-3 md:mb-0 md:w-1/3">
+            <div className="m-6 mb-6 w-full px-3 md:mb-0 md:w-1/3">
               <div className="my-10">
                 <div className="flex justify-between">
                   <button className="mr-5 inline-block rounded-full border-2 border-white px-12 py-2 font-semibold hover:bg-white hover:text-[#0082EF]" onClick={()=>addProject()}>
