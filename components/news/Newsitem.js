a897a398df5a548933115cd8fa0c76040be9c907
Vraj <vrajdesai82912@gmail.com>
2022-03-27 06:20:01 +0530

    ab

--- components/news/Newsitem.js
+++ components/news/Newsitem.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 const NewsItem = ({ data }) => {
   return (
-    <div className="flex">
+    <div className="flex items-end">
       <Link href={data.url}>
-        <div className="m-10 flex p-10">
-          <div class="w-200 max-w-sm overflow-hidden rounded-xl bg-gray-700 shadow-lg">
+        <div className="flex flex-col-2 w-full">
+          <div class=" w-200  max-w-sm  overflow-hidden rounded-xl bg-gray-700 shadow-lg mb-40px">
             <div class="px-6 py-4">
               <div class="text-blue mb-2 text-xl text-white">{data.title}</div>
-              <p class="text-base text-gray-300">{data.source.name}</p>
+              <p class="text-base text-gray-300 ">{data.source.name}</p>
             </div>
           </div>
         </div>
